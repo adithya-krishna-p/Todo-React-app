109e6b1f8f76aca44a8e50489efb8b5de34507eb
adithya-krishna-p <adhii.krishh@gmail.com>
2024-07-28 21:42:09 +0530

    updated

--- src/Todo-brototype/Todo.js
+++ src/Todo-brototype/Todo.js
@@ -1,74 +1,87 @@
-import React, { useContext } from 'react'
-import '../Todo-brototype/Todo.css'
-import { useState } from 'react'
+import React, { useContext } from "react";
+import "../Todo-brototype/Todo.css";
+import { useState } from "react";
 
 const Todo = () => {
-    const [toDos, setToDos] = useState([]);
-    const [toDo, setToDo] = useState('');
-    
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
+  const [toDoDelete, setTodoDelete] = useState("");
 
-  
-    return (
-        <>
-            <div className="app">
-                <div className="mainHeading">
-                    <h1>ToDo List</h1>
+  const deleteTodo = (id) => {
+    const newTodos = toDos.filter(toDo => toDo.id !== id);
+    setToDos(newTodos);
+  };
+
+  return (
+    <>
+      <div className="app">
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <input
+            value={toDo}
+            onChange={(e) => setToDo(e.target.value)}
+            type="text"
+            placeholder="🖊️ Add item..."
+          />
+          <i
+            onClick={() =>
+              setToDos([
+                ...toDos,
+                { id: Date.now(), text: toDo, status: false },
+              ])
+            }
+            className="fas fa-plus"
+          ></i>
+        </div>
+
+        <div className="todos">
+          {toDos.map((obj) => {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input
+                    onChange={(e) => {
+                      console.log(e.target.value);
+                      console.log(obj);
+                      setToDos(
+                        toDos.filter((obj2) => {
+                          if (obj2.id === obj.id) {
+                            obj2.status = e.target.checked;
+                          }
+
+                          return obj2;
+                        })
+                      );
+                    }}
+                    value={obj.status}
+                    type="checkbox"
+                    name=""
+                    id=""
+                  />
+                  <p>{obj.text}</p>
                 </div>
-                <div className="subHeading">
-                    <br />
-                    <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+                <div className="right">
+                  <i onClick={() => deleteTodo()} className="fas fa-times"></i>
                 </div>
-                <div className="input">
-                    <input value={toDo} onChange={(e) => setToDo(e.target.value)}
-                        type="text" placeholder="🖊️ Add item..." />
-                    <i onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} className="fas fa-plus"></i>
-                </div>
-
-                <div className="todos">
-                    {toDos.map((obj) => {
-
-                        return (
-                            <div className="todo">
-                                <div className="left">
-                                    <input onChange={(e) => {
-                                        console.log(e.target.value)
-                                        console.log(obj)
-                                        setToDos(toDos.filter(obj2 => {
-                                            if (obj2.id === obj.id) {
-                                                obj2.status = e.target.checked
-                                            }
-
-                                            return obj2
-                                        }))
-
-
-                                    }} value={obj.status} type="checkbox" name="" id="" />
-                                    <p>{obj.text}</p>
-                                </div>
-                                <div className="right">
-                                    <i  className="fas fa-times"></i>
-                                </div>
-                            </div>)
-                    })}
-
-                    return(<h1 className='activetasks'>Active tasks</h1>)
-
-
-                    {toDos.map((obj) => {
-                        if (obj.status) {
-                            return (<h1>{obj.text}</h1>)
-
-                        }
-                    })}
-
-
-                  
-
-
-                </div>
-            </div>
-        </>
-    )
-}
+              </div>
+            );
+          })}
+          return(<h1 className="activetasks">Active tasks</h1>)
+          {toDos.map((obj) => {
+            if (obj.status) {
+              return <h1>{obj.text}</h1>;
+            }
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Todo;
